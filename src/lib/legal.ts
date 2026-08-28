e69fe84691ed472b6f1b3ec544f4c66edd8c227b
a.a.kulyabin <a.a.kulyabin@gmail.com>
2026-08-28 20:49:24 +0300

    Обновить формулировки юридических документов

--- src/lib/legal.ts
+++ src/lib/legal.ts
@@ -1,7 +1,7 @@
 /** Версии юридических документов, которые фиксируются вместе с заявкой. */
-export const PERSONAL_DATA_CONSENT_VERSION = '2026-08-20'
-export const PRIVACY_POLICY_VERSION = '2026-08-20'
-export const LEGAL_DOCUMENT_EFFECTIVE_DATE = '20 августа 2026 г.'
+export const PERSONAL_DATA_CONSENT_VERSION = '2026-08-28'
+export const PRIVACY_POLICY_VERSION = '2026-08-28'
+export const LEGAL_DOCUMENT_EFFECTIVE_DATE = '28 августа 2026 г.'
 
 /** Срок для обращений, которые не перешли в договор. */
 export const LEAD_RETENTION_DAYS = 180
